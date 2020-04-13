3b11bfcf845f74d7630cbee1682e9db5cd732195
Simon1511 <simon2002.schoenmackers@gmail.com>
2020-04-13 19:40:04 +0200

    manifest: Binderize keymaster

--- common.mk
+++ common.mk
@@ -131,7 +131,8 @@ PRODUCT_PACKAGES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     keystore.exynos5 \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # SamsungDoze
 PRODUCT_PACKAGES += \
