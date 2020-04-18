8b41f49e291ac9683529ba8a2168a86e87f14a2a
Simon1511 <simon2002.schoenmackers@gmail.com>
2020-04-18 19:30:29 +0200

    universal7880: Binderize vibrator

--- common.mk
+++ common.mk
@@ -307,7 +307,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
