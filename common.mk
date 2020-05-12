f8aa422d2353feb2e894fc7e2b4771b190760d08
Simon1511 <simon2002.schoenmackers@gmail.com>
2020-05-12 10:25:54 +0200

    universal7880: Fix camera

--- common.mk
+++ common.mk
@@ -104,8 +104,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
     libexynoscamera_shim \
     libcamera_client_shim \
     Camera2
