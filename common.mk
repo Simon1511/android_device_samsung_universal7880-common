6794678763028c6fbc51d3390c9305e455ae50ef
filiprrs <filiprrs@gmail.com>
2020-07-23 21:34:59 +0200

    universal7880: Pull RIL blobs from G960F Q firmware
    Although it's from a different SoC family, this is still a good idea.
    Resolves SignalStrength issues we have while using A720S Pie blobs.
    
    Pulled from a OneUI 2.0 firmware. OneUI 2.1 adds Samsung security dsms
    blobs as a dependency, which we prefer to avoid, if possible.
    
    Change-Id: I1cd3b46db2f19756927197e793f9b4ed8dcf2bd4

--- common.mk
+++ common.mk
@@ -127,6 +127,8 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.4 \
+    android.hardware.radio.config@1.2 \
     android.hardware.radio@1.3-radio-service.samsung \
     libxml2 \
     libprotobuf-cpp-full
