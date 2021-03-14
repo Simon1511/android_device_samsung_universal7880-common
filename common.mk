2aae15b9a089dca733ebc5b2ec6acf07f7e7991c
Wang Han <416810799@qq.com>
2021-03-14 23:54:58 +0100

    universal7880: Remove deprecated ipv6 tethering support
     * This is not a thing anymore since N.
    
    Change-Id: I944115cc8a33bd3ea21cb9c64c9c98a97d8f0373

--- common.mk
+++ common.mk
@@ -151,12 +151,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     SamsungDoze
 
-# IPv6
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes \
-    libebtc
-
 # Versioned netutils
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
